9ec042264cfadd19c56c1952c4863bb95f89b2f0
Dohoon Kim <70815390+hgmhc@users.noreply.github.com>
2021-01-14 14:13:49 +0900

    Update template_code.cpp

--- template_code.cpp
+++ template_code.cpp
@@ -1,4 +1,3 @@
-//#include <bits/stdc++.h>
 #include <iostream>
 #include <algorithm>
 #include <cmath>
@@ -8,7 +7,9 @@
 #include <vector>
 #include <stack>
 #include <queue>
+#include <deque>
 #include <set>
+#include <bitset>
 #include <map>
 #include <unordered_map>
 #include <memory>
@@ -27,13 +28,32 @@ using pi = pair<int,int>;
 #define REPh(i,a,b) for (int i = a; i < b; ++i)
 #define REP1(i,n) REPc(i,1,n)
 #define REP0(i,n) REPh(i,0,n)
-const int INF = INT_MAX;
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define AC(x) accumulate
+/* for Geometry
+#include <complex>
+#define X real()
+#define Y imag()
+#define cross(u,v) (conj(u)*(v)).Y
+using cll = complex<ll>;
+using cld = complex<ld>;
+*/
+/* for Index Set
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using indexSet = tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>;
+#define fbo find_by_order
+#define ook order_of_key
+*/
+const int INF = INT_MAX, MOD = 1e9+7;
 const ll INFll = LLONG_MAX;
 
-void solve() {
-    
-}
+void solve() {}
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
+    int t; cin >> t;
+    while (t--) solve();
+    return 0;
 }
