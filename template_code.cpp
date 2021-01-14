979df7ed509fcb27869fa1cd2ace715fcfaa21bd
Dohoon Kim <70815390+hgmhc@users.noreply.github.com>
2021-01-14 14:01:25 +0900

    Create template_code.cpp

---
+++ template_code.cpp
@@ -0,0 +1,40 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <cassert>
+#include <functional>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <memory>
+#include <tuple>
+#include <cctype>
+#include <numeric>
+using namespace std;
+using ll = long long;
+using pi = pair<int,int>;
+#define F first
+#define S second
+#define PB push_back
+#define EB emplace_back
+#define end '\n'
+#define REPc(i,a,b) for (int i = a; i <= b; ++i)
+#define REPh(i,a,b) for (int i = a; i < b; ++i)
+#define REP1(i,n) REPc(i,1,n)
+#define REP0(i,n) REPh(i,0,n)
+const int INF = 2.14e9;
+
+void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int x = 100;
+    cout << x;
+}
