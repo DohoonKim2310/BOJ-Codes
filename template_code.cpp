d1001897c63b876c40a35d2234a1adab0abc1a75
Dohoon Kim <70815390+hgmhc@users.noreply.github.com>
2021-01-14 14:04:46 +0900

    Update template_code.cpp

--- template_code.cpp
+++ template_code.cpp
@@ -15,6 +15,7 @@
 #include <tuple>
 #include <cctype>
 #include <numeric>
+#include <climits>
 using namespace std;
 using ll = long long;
 using pi = pair<int,int>;
@@ -26,7 +27,8 @@ using pi = pair<int,int>;
 #define REPh(i,a,b) for (int i = a; i < b; ++i)
 #define REP1(i,n) REPc(i,1,n)
 #define REP0(i,n) REPh(i,0,n)
-const int INF = 2.14e9;
+const int INF = INT_MAX;
+const ll INFll = LLONG_MAX;
 
 void solve() {
     
@@ -34,6 +36,4 @@ void solve() {
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
-    int x = 100;
-    cout << x;
 }
